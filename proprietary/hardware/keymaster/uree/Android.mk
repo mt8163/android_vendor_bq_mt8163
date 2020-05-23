02c57d9bad3f306d76618c33f3b523d1dd3a72e6
R0rt1z2 <rogerortizleal@gmail.com>
2020-05-23 15:00:49 +0200

    aquaris_m8: Build KeyMaster HAL from source.
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

---
+++ proprietary/hardware/keymaster/uree/Android.mk
@@ -0,0 +1,21 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libtz_uree
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := \
+     uree.c \
+     uree_mem.c \
+     dump_sec_mem.c \
+     uree_dbg.c
+
+LOCAL_C_INCLUDES += \
+    $(LOCAL_PATH)/include
+
+LOCAL_CFLAGS += -Wall -Wno-unused-parameter -Werror
+LOCAL_SHARED_LIBRARIES += liblog
+
+include $(BUILD_SHARED_LIBRARY)
+
